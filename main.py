b84c5acaa7dd7104ce9430fc227cd560464e2679
Sweeton Fernandes <37552869+SNFernandes24@users.noreply.github.com>
2022-04-20 12:22:38 +0100

    added new functionality to get lowest rental with tests

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import csv
 
 
 from modules.getRentalTotalWithLease import getRentalTotalWithLease
+from modules.getLowestRental import getLowestRental
 
 if __name__ == '__main__':
     with open('dataset.csv', 'r', newline="") as csvfile:
@@ -11,3 +12,10 @@ if __name__ == '__main__':
         print("Enter Lease number to get rental total for it: ")
         lease = input()
         print("Rental total for lease of {lease}: ".format(lease=lease) + str(getRentalTotalWithLease(reader, int(lease))))
+        
+        print("Enter the number of lowest rentals to fetch: ")
+        count = input()
+        listOfRentals = getLowestRental(reader, int(count))
+        print("{count} lowest rentals: ".format(count=count))
+        for rental in listOfRentals:
+            print(rental)
