5ba5d66f19743afd628c1c9f2ed6f198a174c458
Sweeton Fernandes <37552869+SNFernandes24@users.noreply.github.com>
2022-04-20 14:22:55 +0100

    added debugging to main to test the function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import csv
-
+from datetime import datetime
 
 from modules.getRentalTotalWithLease import getRentalTotalWithLease
 from modules.getLowestRental import getLowestRental
 from modules.mastsOwnedPerTenant import mastsOwnedPerTenant
+from modules.rentalsForDateRange import rentalsForDateRange
 
 if __name__ == '__main__':
     with open('dataset.csv', 'r', newline="") as csvfile:
@@ -21,7 +22,22 @@ if __name__ == '__main__':
         # for rental in listOfRentals:
         #     print(rental)
         
-        print("Amount of Masts owned per tenant: ")
-        print(mastsOwnedPerTenant(reader))
+        # print("Amount of Masts owned per tenant: ")
+        # print(mastsOwnedPerTenant(reader))
+
+        print("Enter a Start Date for rentals within that range(Format: 01 Jan 1990): ")
+        startDate = input()
+        print("Enter a End Date for rentals within that range(Format: 01 Jan 1990): ")
+        endDate = input()
+        listOfDates = rentalsForDateRange(reader, startDate, endDate)
+
+        # Change date format to 01/01/1990
+        for date in listOfDates:
+            date["Lease Start Date"] = datetime.strftime(datetime.strptime(
+            date["Lease Start Date"], '%d %b %Y'), '%d/%m/%Y')
+
+            date["Lease End Date"] = datetime.strftime(datetime.strptime(
+            date["Lease End Date"], '%d %b %Y'), '%d/%m/%Y')
+        print(listOfDates)
 else:
     raise KeyError("Could not load the file or read as Dictionary")
